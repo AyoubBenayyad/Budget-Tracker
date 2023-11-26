54ef70427262dce78b2cd5e8a30fbcda4e36167f
Ibrahim-benzekri <ibrahimbenzekri90@gmail.com>
2023-11-26 01:28:33 +0100

    ayoub commits 1.

---
+++ app/src/main/java/com/example/budgettracker/AddTransactionActivity.kt
@@ -0,0 +1,60 @@
+package com.example.budgettracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class AddTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_transaction)
+
+        val addTransactionBtn:Button = findViewById(R.id.addTransactionBtn)
+        addTransactionBtn.setOnClickListener {
+            val labelInput: TextInputEditText = findViewById(R.id.labelInput)
+            val descriptionInput:TextInputEditText= findViewById(R.id.descriptionInput)
+            val amountInput:TextInputEditText= findViewById(R.id.amountInput)
+
+
+            val label = labelInput.text.toString()
+            val description = descriptionInput.text.toString()
+            val amount = amountInput.text.toString().toDoubleOrNull()
+
+            val labelLayout: TextInputLayout = findViewById(R.id.labelLayout)
+            val amountLayout: TextInputLayout = findViewById(R.id.labelLayout)
+            if(label.isEmpty())
+                labelLayout.error = "Please enter a valid label"
+            else if(amount == null)
+                amountLayout.error = "Please enter a valid amount"
+            else {
+
+            }
+        }
+        val amountInput:TextInputEditText= findViewById(R.id.amountInput)
+        val labelInput: TextInputEditText = findViewById(R.id.labelInput)
+        val labelLayout: TextInputLayout = findViewById(R.id.labelLayout)
+        val amountLayout: TextInputLayout = findViewById(R.id.labelLayout)
+        labelInput.addTextChangedListener {
+            if(it!!.isNotEmpty())
+                labelLayout.error = null
+        }
+
+        amountInput.addTextChangedListener {
+            if(it!!.isNotEmpty())
+                amountLayout.error = null
+        }
+
+
+        val closeBtn: ImageButton = findViewById(R.id.closeBtn)
+        closeBtn.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    
+}
--- app/src/main/java/com/example/budgettracker/MainActivity.kt
+++ app/src/main/java/com/example/budgettracker/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.budgettracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var transactions: ArrayList<Transaction>
+    private lateinit var transactionAdapter:TransactionAdapter
+    private lateinit var linearlayoutManager:LinearLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        transactions = arrayListOf(
+        Transaction("Weekend budget",400.00),
+        Transaction("Bananas", -4.00),
+        Transaction("Gasoline", -40.90),
+        Transaction("Breakfast",  -9.99),
+        Transaction("Water bottles", -4.00),
+        Transaction("Suncream",-8.00),
+        Transaction("Car Park", -15.00)
+        )
+
+        transactionAdapter = TransactionAdapter(transactions)
+        linearlayoutManager = LinearLayoutManager(this)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
+
+        recyclerView.apply {
+            adapter = transactionAdapter
+            layoutManager=linearlayoutManager
+        }
+        updateDashboard()
+
+        val addBtn: FloatingActionButton= findViewById(R.id.addBtn)
+
+        addBtn.setOnClickListener {
+            val intent = Intent(this, AddTransactionActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun updateDashboard(){
+        val totalAmount = transactions.map { it.amount }.sum()
+        val budgetAmount = transactions.filter { it.amount>0 }.map{it.amount}.sum()
+        val expenseAmount = totalAmount - budgetAmount
+        val balance: TextView = findViewById(R.id.balance)
+        val budget: TextView = findViewById(R.id.budget)
+        val expense: TextView = findViewById(R.id.expense)
+        balance.text = "$ %.2f".format(totalAmount)
+        budget.text = "$ %.2f".format(budgetAmount)
+        expense.text = "$ %.2f".format(expenseAmount)
     }
 }
